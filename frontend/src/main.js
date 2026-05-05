2367824eaf54d89ac392f5750ba2a2bcd0cb2881
Esteban Radesca <estebanfranciscoradesca@hotmail.com>
2026-05-05 00:01:38 -0300

    Integración de la cámara en la página web

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 // Importo la variable de entorno de la dirección del Weboscket
 const WS_URL = import.meta.env.VITE_WS_URL
-
+const CAMARA_URL = import.meta.env.VITE_CAMARA_URL
 const ws = new WebSocket(WS_URL);
 
 // Probando cosas
@@ -25,12 +25,12 @@ ws.addEventListener("message", (evento) => {
     if (datos.CA) {
         document.getElementById('caudal_valor').innerText = datos.CA;
     } else {
-        document.getElementById('caudal_valor').innerText = "No funciona";
+        document.getElementById('caudal_valor').innerText = "Sin datos";
     }
     if (datos.T) {
         document.getElementById('temp_valor').innerText = datos.T;
     } else {
-        document.getElementById('temp_valor').innerText = "No funciona";
+        document.getElementById('temp_valor').innerText = "Sin datos";
     }
     
     if (datos.R == "1") {
@@ -39,6 +39,44 @@ ws.addEventListener("message", (evento) => {
         document.getElementById('rele_estado').innerText = "Apagado";        
     }
 });
+
+
+async function conectar() {
+    try {   
+        const pc = new RTCPeerConnection();
+        pc.addEventListener('track', (evento) => {
+            const webrtc_stream = document.getElementById('stream');
+            webrtc_stream.srcObject = evento.streams[0];
+        });
+
+        pc.addTransceiver("video", { direction: "recvonly"});
+
+        const oferta = await pc.createOffer();
+        await pc.setLocalDescription(oferta);
+
+        const respuesta = await fetch(CAMARA_URL, {
+            method: "POST",
+            body: oferta.sdp,
+            headers: {"Content-Type": "application/sdp" }
+        });
+
+        if (!respuesta.ok) throw new Error("Error en el servidor WHEP");
+
+        const respuestaSdp = await respuesta.text();
+
+        await pc.setRemoteDescription({ type: "answer", sdp: respuestaSdp });
+            
+        console.log("Conexión WebRTC establecida con éxito");
+
+    } catch (error) {
+        console.error("Fallo al conectar:", error);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    conectar();
+});
+     
 
 
 
@@ -66,24 +104,9 @@ async function obtenerDatos() {
         body: JSON.stringify({id_equipo: 1, estado: "encendido"})
     });
 
-}
-/*
-async function obtenerDatos2() {
-    const url = 'http://localhost:8000/equipo/estado';
+};
 
-    const respuesta = await fetch(url, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({id_equipo: 2, estado: "apagado"})
-    });
-
-}
-*/
 const info = document.getElementById('conectar');
 
 info.onclick = obtenerDatos;
 
-
-//const info2 = document.getElementById('apagar');
-
-//info2.onclick = obtenerDatos2;
